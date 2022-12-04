785c620242f52385902268dd26d82e67502920fa
r-tarnowski <radek.github@wp.pl>
2022-12-05 00:16:55 +0100

    Example from lesson 1.08 added

---
+++ moduly.py
@@ -0,0 +1,26 @@
+import math
+a = 3
+b = -3
+print(abs(a))
+print(abs(b))
+
+c = 3.1
+print(round(c))
+
+d = 3.56
+print(round(d))
+
+e = 3.12345
+print(round(e, 4))
+
+print(f"wartość: {d}, floor: {math.floor(d)}")
+
+print(f"wartość: {math.pi}, sinus: {math.sin(math.pi)}")
+print(f"wartość: {math.pi}, round(sinus,6): {round(math.sin(math.pi), 6)}")
+
+print(f"wartość: {math.pi}, ceil: {math.ceil(math.pi)}")
+
+print(f"wartość: {math.e}, ceil: {math.ceil(math.e)}")
+
+x = 25
+print(f"wartość: {x}, sqrt: {math.sqrt(x)}")
