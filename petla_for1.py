7056ff4ec2d10cc6c95fd2e62e1ca2ec1bdd058e
r-tarnowski <radek.github@wp.pl>
2022-12-10 19:38:21 +0100

    Files for lesson 1,09 added: example and task

---
+++ petla_for1.py
@@ -0,0 +1,12 @@
+for zmienna in range(-10, 10):
+    print(zmienna, end=", ")
+
+print()
+
+for zmienna in range(-1000, 1001):
+    print(zmienna, end=", ")
+    
+print()
+
+for licznik in range(10):
+   print(f"{licznik} Ala ma kota")
