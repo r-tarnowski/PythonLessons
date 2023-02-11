e27c111d5b4b435da4cdbeb9b944db6c1c0c9479
r-tarnowski <radek.github@wp.pl>
2023-02-11 19:26:18 +0100

    Exercise for lesson 2.14 improved

--- zadanie_bez_numeru3.py
+++ zadanie_bez_numeru3.py
@@ -1,11 +1,11 @@
 from random import randint
 numbers = [int(number) for number in input().split()]
-line = []
 for i in range(numbers[3]):
+    line = []
     for j in range(numbers[2]):
         line.append(randint(numbers[0], numbers[1]))
     print(line)
-    line.clear()
+
 
 
 
