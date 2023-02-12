87d8001435beba41c37933f483d31081967024e1
r-tarnowski <radek.github@wp.pl>
2023-02-12 15:50:41 +0100

    Example for lesson 3.1 added

---
+++ functions.py
@@ -0,0 +1,6 @@
+from my_function import function_name
+
+function_name()
+numbers = [int(x) for x in input("Podaj liczby z których chcesz wyliczyć średnią: ").split()]
+print(sum(numbers)/len(numbers))
+function_name()
